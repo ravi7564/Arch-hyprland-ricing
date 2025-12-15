90f22923bda9b53c9fd6cfdd3330058b1c17a0c6
ravi <143782095+ravi7564@users.noreply.github.com>
2025-12-15 18:05:07 +0530

    Add files via upload

--- rofi/powermenu/type-1/powermenu.sh
+++ rofi/powermenu/type-1/powermenu.sh
@@ -1,110 +1,88 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Rofi   : Power Menu
-#
-## Available Styles
-#
-## style-1   style-2   style-3   style-4   style-5
-
-# Current Theme
+# =========================
+# Config
+# =========================
 dir="$HOME/.config/rofi/powermenu/type-1"
-theme='style-1'
+theme="style-1"
 
-# CMDs
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+uptime="$(uptime -p | sed 's/up //')"
+host="$(hostname)"
 
-# Options
-shutdown=' Shutdown'
-reboot=' Reboot'
-lock=' Lock'
-suspend=' Suspend'
-logout=' Logout'
-yes=' Yes'
-no=' No'
+# =========================
+# Icons & Labels
+# =========================
+shutdown="  Shutdown"
+reboot="  Reboot"
+lock="  Lock"
+suspend="  Suspend"
+logout="  Logout"
 
-# Rofi CMD
+yes="  Yes"
+no="  No"
+
+# =========================
+# Rofi Commands
+# =========================
 rofi_cmd() {
-	rofi -dmenu \
-		-p "$host" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+  rofi -dmenu \
+    -p "$host" \
+    -mesg "Uptime: $uptime" \
+    -theme "${dir}/${theme}.rasi"
 }
 
-# Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
+  rofi -dmenu \
+    -p "Confirmation" \
+    -mesg "Are you sure?" \
+    -theme-str 'window { width: 260px; }' \
+    -theme-str 'listview { columns: 2; lines: 1; }' \
+    -theme-str 'element-text { horizontal-align: 0.5; }' \
+    -theme "${dir}/${theme}.rasi"
 }
 
-# Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+  printf "%s\n%s" "$yes" "$no" | confirm_cmd
 }
 
-# Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+  printf "%s\n%s\n%s\n%s\n%s" \
+    "$lock" "$suspend" "$logout" "$reboot" "$shutdown" | rofi_cmd
 }
 
-# Execute Command
-run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
-		fi
-	else
-		exit 0
-	fi
-}
-
+# =========================
 # Actions
+# =========================
+run_cmd() {
+  [[ "$(confirm_exit)" != "$yes" ]] && exit 0
+
+  case "$1" in
+    shutdown)
+      systemctl poweroff
+      ;;
+    reboot)
+      systemctl reboot
+      ;;
+    suspend)
+      playerctl pause 2>/dev/null
+      pactl set-sink-mute @DEFAULT_SINK@ toggle
+      systemctl suspend
+      ;;
+    logout)
+      hyprctl dispatch exit
+      ;;
+  esac
+}
+
+# =========================
+# Main
+# =========================
 chosen="$(run_rofi)"
-case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $lock)
-		if [[ -x '/usr/bin/hyprlock' ]]; then
-			hyprlock
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+
+case "$chosen" in
+  "$shutdown") run_cmd shutdown ;;
+  "$reboot")   run_cmd reboot ;;
+  "$lock")     hyprlock ;;
+  "$suspend")  run_cmd suspend ;;
+  "$logout")   run_cmd logout ;;
 esac
